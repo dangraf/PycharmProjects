64e0a650d74eabf820a3c9271c5c309c89baa1be
daniel <dangraf@hotmail.com>
2017-06-17 22:05:14 +0200

    added python 3 support

--- firstTestProject/Ticker/ticker_base.py
+++ firstTestProject/Ticker/ticker_base.py
@@ -1,6 +1,7 @@
 import datetime as dt
 from time import sleep
-import urllib2
+import urllib.request as urlreq
+import urllib.error as urlerr
 import json
 from pandas.io.json import json_normalize
 import pandas as pd
@@ -31,22 +32,26 @@ class ticker_base:
         self.LastUpdate = dt.datetime.now()
 
     def getData(self):
-        req = urllib2.Request(self.url,headers={'User-Agent' : "Magic Browser"})
         df_ = pd.DataFrame()
         df = df_.fillna(0)  # with 0s rather than NaNs
+        req = urlreq.Request(url=self.url, headers={'User-Agent' : "Magic Browser"})
 
         try:
-            response = urllib2.urlopen(req)
-        except urllib2.HTTPError as e:
-            print 'The server couldn\'t fulfill the request.'
-            print 'Error code: ', e.code
-        except urllib2.URLError as e:
-            print 'We failed to reach a server.'
-            print 'Reason: ', e.reason
+            response = urlreq.urlopen(req)
+
+        except urlerr.HTTPError as e:
+            print('error on url:',self.url)
+            print( 'The server couldn\'t fulfill the request.')
+            print ('Error code: ', e.code)
+        except urlerr.URLError as e:
+            print('error on url:', self.url)
+            print('We failed to reach a server.')
+            print('Reason: ', e.reason)
         else:
             the_page = response.read()
+            encoding = response.info().get_content_charset('utf-8')
             try:
-                data = json.loads(the_page)
+                data = json.loads(the_page.decode(encoding))
                 df = json_normalize(data)
             except:
                 print('unable to parse data from homepage to pandas')
@@ -56,22 +61,24 @@ class ticker_base:
     def run(self):
         while self.running:
             try:
-                self.data = pd.read_pickle(self.filename)
+                allData = pd.read_pickle(self.filename)
             except:
                 print('unable to load file')
-                self.data = []
+                allData = []
 
             df = self.getData()
-            if( len(self.data) == 0):
-                self.data = df
+            if( len(allData) == 0):
+                allData = df
             else:
-                self.data = self.data.append(df)
-            self.data.to_pickle(self.filename)
+                allData = allData.append(df)
+            allData.to_pickle(self.filename)
 
-            #pickle.dump(self.data, open( self.filename, "wb" ))
+
             self.iterator +=1
-            print(self.data.shape)
-
+            print(allData.shape)
+             #clear memory
+            temp = [allData]
+            del temp
             self.waitUntilNextUpdate()
 
 
