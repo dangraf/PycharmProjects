dff14cc00f98b3c68cd8a9dcc866ab5166c93e09
daniel <dangraf@hotmail.com>
2017-07-11 22:22:56 +0200

    code is working but no error handling. bad names on variables, need renaming.

--- firstTestProject/mailposi/mailposi.py
+++ firstTestProject/mailposi/mailposi.py
@@ -1,12 +1,17 @@
+# -*- coding: utf-8 -*-
 import smtplib
 from email.mime.text import MIMEText
 import configparser
 import random
+import schedule
+import time
+
 
 class Settings:
     def __init__(self):
         self.parameters = None
         self.loadSettings()
+
 
     def loadSettings(self):
         try:
@@ -16,26 +21,26 @@ class Settings:
             print('error loadSettings in mailposi.py')
             pass
     def composeTextMsg(self):
+        # List all sections in the file
+        # remove settings and mailing list that does not contain any questions
         sections = self.parameters.sections()
         sections.remove('settings')
         sections.remove('maillist')
         text = ""
 
+
+        # iterate thrugh each section and radnomize a sentence within all options.
         for sec in sections:
             options = self.parameters.options(sec)
             idx = random.randint(0, len(options)-1)
-            text = text + self.parameters.get(sec,options[idx]) + " \n \n"
-
-            # generate a random number between 0 and len(options)
-
-
+            text = text +sec +':\n'
+            text = text + '-'+ self.parameters.get(sec,options[idx]) + " \n \n"
         return text
 
 
     def sendMail(self):
+        self.loadSettings()
         me = self.parameters.get('settings','mailaddress')
-
-
         s = smtplib.SMTP(self.parameters.get('settings', 'popserver'),\
                          self.parameters.get('settings', 'port'))
         s.ehlo()  # Hostname to send for this command defaults to the fully qualified domain name of the local host.
@@ -54,5 +59,15 @@ class Settings:
             #s.sendmail(me, receiver, msg.as_string())
         s.quit()
 
-set = Settings()
-print( set.composeTextMsg() )
+    def run(self):
+        sendHour = str(self.parameters.get('settings', 'passowrd'))
+
+        while(1):
+            schedule.every().day.at(sendHour).do(self.sendMail)
+            time.sleep(60)
+
+
+
+if __name__ == "__main__":
+    set = Settings()
+    set.run()
