aec08aa32fa6d237201f058e4dddf274f74247f3
daniel <dangraf@hotmail.com>
2017-07-15 00:18:15 +0200

    re-written large parts of the code. Removed "filter" parts that might not be used in the future.  added doc-strings documentation, added validation of input parameters

--- firstTestProject/importData/importCSV.py
+++ firstTestProject/importData/importCSV.py
@@ -1,223 +1,122 @@
 import numpy as np
 import pandas as pd
-import json
-
-def subinterpolate( stepInSec,timestamp,price,volume ):
-
-    outputTimestamp = []
-    outputBid = []
-    outputAmount = []
-    #expand/collaps array to 1sec ticks
-    for i in range(len(timestamp)-1):
-        tm = range(timestamp[i],timestamp[i+1],1)
-        if(len(tm)>1):
-            outputTimestamp.extend(tm)
-            temp = np.empty(len(tm))
-            temp.fill(price[i])
-            outputBid.extend(temp)
-            temp.fill(0)
-            temp[0] = volume[i]
-            outputAmount.extend(temp)
-    #fill up array to be equally divided into stepInSec, chunks
-    mod = len(outputTimestamp)%stepInSec
-    fill = stepInSec - mod
-
-    tm = range(timestamp[-1],timestamp[-1]+fill,1)
-    outputTimestamp.extend(tm)
-    temp = np.empty(len(tm))
-    temp[0] = price[-1]
-    outputBid.extend(temp)
-    temp[0] = volume[-1]
-    outputAmount.extend(temp)
-    # create New shapes
-    rsAmount = np.reshape(outputAmount,(-1,stepInSec))
-    rsTime = np.reshape(outputTimestamp,(-1,stepInSec))
-    rsBid = np.reshape(outputBid,(-1,stepInSec))
-
-    ouputLen = len(outputTimestamp)/stepInSec
-#    rsBid = np.reshape(price,(stepInSec,-1))
-
-    outputTm = rsTime[:,fill-1]
-
-    outputAm = np.zeros(ouputLen)
-    outputBid =np.zeros(ouputLen)
-
-    for i in range(stepInSec):
-        outputBid += rsBid[:,i]*rsAmount[:,i]
-        outputAm += rsAmount[:,i]
-
-
-    for i in range(ouputLen):
-        if(outputAm[i] <0.00001):
-            outputBid[i] = outputBid[i-1]
-        else:
-            outputBid[i] = outputBid[i]/outputAm[i]
-
-    return [outputTm, outputBid, outputAm]
-
-
-def readCsv( filePath, timeStepInSec  ):
-    f = open( filePath,'r')
-    d = f.readlines()
-    f.close()
-
-    time = []
-    last =[]
-    volume = []
-#temp = []
-
-
-
-    for row in d[1:]:
-
-            v = row.split(',')[2] #volume
-            r = row.split(',')[1] #last
-            t = row.split(',')[0] #time
-
-            last.append( float(r) )
-            volume.append( float(v))
-            time.append( int(float(t)))
-#           endIndex = offset+chunkSize
-
-    return subinterpolate( timeStepInSec,time,last,volume )
-
-def getFutureFiltered( dataFrame,myFilter ):
-
-    filtchange = np.convolve(dataFrame['price'],myFilter,'valid')
-#    price = np.array(price[:-(len(myFilter)-1)])
-
-    filtchange = np.divide(filtchange,dataFrame['price'][:-len(myFilter)+1]) - 1.0
-    myindex = dataFrame.index
-    myindex = myindex[:-len(myFilter)+1]
-
-    dataFrame['FutureFilter'] = pd.Series(filtchange,index = myindex)
-
-#    volume = np.array(volume[:-(len(myFilter)-1)])
-#    dates = np.array(dates[:-(len(myFilter)-1)])
-    return dataFrame
-
-def getHistoryFiltered( dataFrame,myFilter ):
-
-    filtchange = np.convolve(dataFrame['price'],myFilter,'valid')
-    print dataFrame['price'][len(myFilter)-1:]
-    filtchange = np.divide(filtchange,dataFrame['price'][len(myFilter)-1:])-1
-
-
-#    myindex = dataFrame.index
-#    myindex = myindex[-len(myFilter)-1:]
-
-    #dataFrame['HistoryFilter'] = pd.Series(filtchange,index = myindex)
-    dataFrame['HistoryFilter'] = filtchange
-    return dataFrame
-
-def getFutureFilter( indexArray):
-    myFilter = np.zeros(np.max(indexArray)+1)
-    for index in indexArray:
-        myFilter[index-1] = 1.0/len(indexArray)
-    return myFilter
-
-def getHistoryFilter( indexArray ):
-    myFilter = np.zeros(np.max(indexArray)+1)
-    for index in indexArray:
-        myFilter[index] = 1.0/len(indexArray)
-
-    return myFilter
+import datetime as dt
+import time
+from pyvalid import accepts, returns
 
 # Calculates orderdepth rato
 # todo,handle price information together with volume information.x
-def getOrderDepthRatio( filename, topNumOrders ):
-    """ testing To get some Infor from it """
-    indexVolume = 1
+#def getOrderDepthRatio( filename, topNumOrders ):
+#    """ testing To get some Infor from it """
+#    indexVolume = 1
 
-    f = open(filename,'r')
-    d = f.readlines()
-    f.close();
+#    f = open(filename,'r')
+#    d = f.readlines()
+#    f.close();
 
-    output = []
+#    output = []
 
-    for r in d:
+#    for r in d:
 
-        data = json.loads(r)
-        bidsvolume = np.array(data['bids'][0:topNumOrders]).astype(float)
-        asksvolume = np.array(data['asks'][0:topNumOrders]).astype(float)
-        timestamp= np.int64(data['date'])
+#        data = json.loads(r)
+#        bidsvolume = np.array(data['bids'][0:topNumOrders]).astype(float)
+#        asksvolume = np.array(data['asks'][0:topNumOrders]).astype(float)
+#        timestamp= np.int64(data['date'])
 #        if( len(output)>1 and  timestamp < output[-1][0]):
 #                print timestamp
 
-        sumBid = sum(bidsvolume[:,indexVolume])
-        sumAsk = sum(asksvolume[:,indexVolume])
-        ratio = (sumBid-sumAsk)/(sumBid+sumAsk)
-        output.append([timestamp,ratio])
+#        sumBid = sum(bidsvolume[:,indexVolume])
+#        sumAsk = sum(asksvolume[:,indexVolume])
+#        ratio = (sumBid-sumAsk)/(sumBid+sumAsk)
+#        output.append([timestamp,ratio])
 
-    out = np.array(output)
+#    out = np.array(output)
 
 #    ratioSeries = pd.Series(out[:,1],index=np.transpose( np.int64(out[:,0])),name='od_ratio' )
-    ratioSeries = pd.DataFrame([out[:,0],out[:,1]]).T
+#    ratioSeries = pd.DataFrame([out[:,0],out[:,1]]).T
 
-    ratioSeries.columns= ['date','od_ratio']
-    ratioSeries['date'] = ratioSeries['date'].astype(np.int64)
+#    ratioSeries.columns= ['date','od_ratio']
+#    ratioSeries['date'] = ratioSeries['date'].astype(np.int64)
 #    ratioSeries.index.name = 'date'
 
-    ratioSeries = ratioSeries.groupby('date').mean()
+#    ratioSeries = ratioSeries.groupby('date').mean()
 
     #ratioArray = pd.DataFrame(out[:,1],index=np.int64(out[:,0]))
 
-    return ratioSeries
-
-# Reads csv Chart using price,  date and volume (no orderdepth)
-# groups volumes into 1sek data. Some dates may be missing when no trading occures.
-def readCsvChart(filenamePrice):
-    tmpPrice = pd.read_csv(filenamePrice)
-    tmpPrice.columns = ['date','price','volume']
-    priceGr = tmpPrice.groupby('date')
-    price = priceGr.agg({'price':np.mean,'volume':np.sum})
+#    return ratioSeries
 
 
-    return price
-#private function,  used to fill create a complete array of price,  volume anad orderdepth
-# with 1sek data
-def fillAndSetData(dataframe,timestampStart=None,timestampEnd=None):
-    #hitta start och slut index
-    #skapa en index-lista for samtilga instanser
-    #lagga ihop bada arrayerna.
+@accepts(int)
+def _timestamp_to_datetime(timestamp):
+    ret = dt.datetime.fromtimestamp(int(timestamp))
+    return ret
 
-    if( timestampStart != None):
-        newIndex = range(timestampStart,timestampEnd)
-        dataframe = pd.DataFrame(dataframe,index = newIndex )
+@accepts(str)
+def _datetime_to_timestamp(myDateString):
+    d_time = dt.datetime.strptime(myDateString, "%Y-%m-%d %H:%M:%S")
+    return int(time.mktime(d_time.timetuple()))
 
-    if 'price' in dataframe.columns:
-        dataframe['price'] = dataframe['price'].ffill()
-    if 'od_ratio' in dataframe.columns:
-        dataframe['od_ratio'] = dataframe['od_ratio'].ffill()
-    if 'volume' in dataframe.columns:
-        dataframe['volume'] = dataframe['volume'].fillna(0)
+@accepts(pd.core.frame.DataFrame, (str,int,None),(str,int,None))
+def _slice_df_by_date(df_in, date_start, date_end):
+
+    df_out = df_in.copy()
+    if type(date_start)== str:
+        date_start = _datetime_to_timestamp(date_start)
+    if type(date_end) == int:
+        date_end = _datetime_to_timestamp(date_end)
+
+    if date_start != None :
+        df_out = df_in.where( df_out['timestamp']>date_start )
+    if date_end != None:
+        df_out = df_out.where(df_out['timestamp']<date_end )
+
+    df_out.dropna(inplace=True)
+
+
+@returns(pd.core.frame.DataFrame)
+@accepts(str, str,(int,str),(int,str))
+def getDataFrameFromHistoricalData(filename, groupInterval = '1min', timeStart=None,timeEnd=None ):
+    """Return a cleanded pandas dataframe
+    Keyword arguments:
+        filename -- filename of CSV file to be converted
+        groupIntervalS -- eg '30s' or '1min' or similar (default 1min
+        timeStart -- unix timestamp or string on format '2011-09-13 15:53:44' (default none)
+        timeEnd -- unix timestamp or string on format '2011-09-13 15:53:44' (default none)
+        """
+    df = pd.read_csv( filename )
+    df.columns = ['timestamp', 'price', 'volume']
+
+    df['datetime'] = pd.to_datetime(df['timestamp'],unit='s')
+    df = df.set_index('datetime')
+    df = df.groupby(pd.TimeGrouper( groupInterval) ).agg({'price':np.mean,'volume':np.sum})
+    df['price'].ffill(inplace=True)
+    df['volume'].fillna(0,inplace=True)
+
+    return df
 
 
 
-    return dataframe
+#def syncPriceAndOrderdepth(timestampStart,timestampEnd,filenamePrice,filenameOrdDepth):
 
-# takes two files with
-def syncPriceAndOrderdepth(timestampStart,timestampEnd,filenamePrice,filenameOrdDepth):
+#    priceData = readCsvChart( filenamePrice )
+#    S_price = pd.Series(priceData['price'])
+#    S_volume = pd.Series(priceData['volume'])
 
-    priceData = readCsvChart( filenamePrice )
-    S_price = pd.Series(priceData['price'])
-    S_volume = pd.Series(priceData['volume'])
+#    ratio = getOrderDepthRatio(filenameOrdDepth,20)
 
-    ratio = getOrderDepthRatio(filenameOrdDepth,20)
-
-    S_ratio = pd.Series(ratio['od_ratio'])
+#    S_ratio = pd.Series(ratio['od_ratio'])
 
 #    S_ratio.to_csv('od_ratio.csv')
 
-    combinedData = pd.DataFrame([S_price,S_volume,S_ratio]).T
-    combinedData.index.name = 'date'
+#    combinedData = pd.DataFrame([S_price,S_volume,S_ratio]).T
+#    combinedData.index.name = 'date'
 
 
-    out = fillAndSetData(combinedData,timestampStart,timestampEnd )
+#    out = fillAndSetData(combinedData,timestampStart,timestampEnd )
 
 
     #print out
-    return out
+#    return out
 
 
 
