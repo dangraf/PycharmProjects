03aff6c4181e5d44e73c8a261c22f41eb361fad5
dangraf <dangraf@hotmail.com>
2019-01-08 21:15:59 +0100

    added info for scraper

--- Projects/mongo_data/crypto_news_data.py
+++ Projects/mongo_data/crypto_news_data.py
@@ -7,8 +7,11 @@ class CryptoNews(Document):
     tags = ListField(StringField(), required=True)
     title = StringField(max_length=200, required=True, unique=True)
     description = StringField(max_length=200, required=True)
-    text = StringField(max_length=200, required=True)
+    text = StringField(required=True)
     url = StringField(max_length=200, required=True)
+    time_posted =DateTimeField(required=False)
+    summay = StringField(required=False)
+    keywords =ListField(StringField(), required=False)
     meta = {
         'db_alias': 'NewsDb',
         'collection': 'CryptoNews'
